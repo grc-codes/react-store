44c1f1589455cba19e0fdb292694cae04106acd4
Andrew Garcia <grc.codes@gmail.com>
2020-09-12 14:23:58 -0400

    added firebase utils, added authenticated users to firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -8,7 +8,7 @@ class SignIn extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state ={
+        this.state = {
             email: '',
             password: ''
         }
