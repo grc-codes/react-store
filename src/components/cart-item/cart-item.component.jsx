dc17a4e77592cc4f0efa9af36a13f1ffcf398cc0
Andrew Garcia <grc.codes@gmail.com>
2020-09-25 08:37:23 -0400

    moved directory and shop data into own reducer; created selectors and components for directory and shop

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,7 +3,7 @@ import './cart-item.style.scss';
 
 const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
     <div className='cart-item'>
-        <img src={imageUrl} />
+        <img src={imageUrl} alt='cart item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>{quantity} x ${price}</span>
